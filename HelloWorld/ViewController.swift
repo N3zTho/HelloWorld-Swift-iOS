0d7cd8048ef517be702851e01d9a69c3286fd685
Ernesto Redonet <ushernew1422@gmail.com>
2020-06-20 20:18:36 -0400

    Apply UiAlertController

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var texto: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func boton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "HelloWorld", message: texto.text, preferredStyle: .alert)
+        let accion = UIAlertAction(title: "ok", style: .default) { (_) in
+            print("click on button")
+        }
+        
+        alert.addAction(accion)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
